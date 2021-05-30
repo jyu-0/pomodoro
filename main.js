e56b84733afe57c425b7fb29e3f2ab36a21e3188
achen-2 <65137522+achen-2@users.noreply.github.com>
2021-05-29 22:43:59 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,3 @@
 
 
-function start(){
-  minutes = 24;
-  seconds = 59;
-  
-  document.getElementById("minutes").innerHTML = minutes;
-  document.getElementById("seconds").innnerHTML = seconds;
-  
-  var minutes_interval = setInterval(minutesTimer, 60000);
-  var seconds_interval = setInterval(secondsTimer, 1000);
-  
-  function minutesTimer(){
-    minutes = minutes-1;
-    document.getElementById("minutes").innerHTML = minutes;
-  }
-  
-  function secondsTimer(){
-    seconds = seconds-1;
-    document.getElementById("seconds").innerHTML = seconds;
-    
-    if(seconds<=0){
-      if(minutes<=0){
-        clearInteval(minutes_interval);
-        clearInterval(seconds_interval);
-      }
-      seconds = 60;
-    }
-  }
-}
+
